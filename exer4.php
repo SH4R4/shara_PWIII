072f0568a516bc79fc0e7157a4ecfa139abf6e3c
Yasai <133717535+SH4R4@users.noreply.github.com>
2025-03-12 16:56:56 -0300

    Update exer4.php

--- exer4.php
+++ exer4.php
@@ -9,10 +9,8 @@ if (is_numeric($num1) && is_numeric($num2)) {
     $quadrado1 = $num1 * $num1;
     $quadrado2 = $num2 * $num2;
 
-    // Soma os quadrados
     $somaQuadrados = $quadrado1 + $quadrado2;
 
-    // Exibe os resultados
     echo "O quadrado de $num1 é: $quadrado1\n";
     echo "O quadrado de $num2 é: $quadrado2\n";
     echo "A soma dos quadrados é: $somaQuadrados\n";
