fd98ac688d3c80d3e9e8e07034c1aa9901788368
Yasai <133717535+SH4R4@users.noreply.github.com>
2025-03-12 16:57:16 -0300

    Update exer3.php

--- exer3.php
+++ exer3.php
@@ -7,8 +7,8 @@ if (is_numeric($valor)) {
     $por5 = $valor * 0.05;
     $por50 = $valor * 0.50;
 
-    echo "5% de $valor é: $porcentagem5\n";
-    echo "50% de $valor é: $porcentagem50\n";
+    echo "5% de $valor é: $por5\n";
+    echo "50% de $valor é: $por50\n";
 } else {
     echo "Por favor, insira um valor numérico válido.\n";
 }
